a7690e287914cd3263888b14a95ed4ffee0cd2f6
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-08-01 19:07:48 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Home() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>The Home Page</h1>
+      <p>Welcome to our website! This is the home page of our application.</p>
+      <p>Feel free to explore the different sections and features.</p>
+    </div>
+  )
+ 
 }
 
 export default Home;
