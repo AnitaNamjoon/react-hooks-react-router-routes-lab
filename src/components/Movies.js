b79a06fce71b369960e0b1a95a781c69ff06eaa8
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-08-01 19:08:11 +0300

    Update Movies.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>List of Movies</h1>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <h2>{movie.title}</h2>
+            <p>Director: {movie.director}</p>
+            <p>Genre: {movie.genre}</p>
+            <p>Year: {movie.year}</p>
+            <p>Duration: {movie.duration} minutes</p>
+            <p>Plot: {movie.plot}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Movies;
